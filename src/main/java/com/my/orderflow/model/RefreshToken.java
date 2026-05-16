0b3614f21d09788b44ee5d3d3fb75fd764372358
OleksandrSaveliev <a.savelev.krm@gmail.com>
2026-05-16 17:48:24 +0300

    refactor refresh token entity: enable jpa auditing

--- src/main/java/com/my/orderflow/model/RefreshToken.java
+++ src/main/java/com/my/orderflow/model/RefreshToken.java
@@ -2,6 +2,8 @@ package com.my.orderflow.model;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -29,4 +31,12 @@ public class RefreshToken {
     private LocalDateTime expiresAt;
 
     private boolean revoked;
+
+    @CreatedDate
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
 }
