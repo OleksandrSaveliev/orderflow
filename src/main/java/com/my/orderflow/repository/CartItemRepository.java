63b5c0f683bae9fc3be8962ff9c7fa5e1081158a
OleksandrSaveliev <a.savelev.krm@gmail.com>
2026-05-17 18:02:52 +0300

    fix: CartItemRepository, add needed property on refresh token migration, add needed db dependency

--- src/main/java/com/my/orderflow/repository/CartItemRepository.java
+++ src/main/java/com/my/orderflow/repository/CartItemRepository.java
@@ -4,11 +4,8 @@ import com.my.orderflow.model.CartItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface CartItemRepository extends JpaRepository<CartItem, UUID> {
-
-    Optional<CartItem> findByCartIdAndProductId(UUID cartId, UUID productId);
 }
